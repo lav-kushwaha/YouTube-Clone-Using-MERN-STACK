b6e9887741537ec84c2fdf88d72ccc2d45fc14a9
Lav Kushwaha <lavkumar1224@gmail.com>
2024-01-20 21:21:52 +0530

    Navbar

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import './Navbar.css'
 import logo from './youtubelogo.png'
 import SearchBar from './SearchBar/SearchBar'
+import {RiVideoAddLine} from 'react-icons/ri'
+import {IoMdNotificationsOutline} from 'react-icons/io'
+import {BiUserCircle} from 'react-icons/bi'
+
 const Navbar = ()=>{
     return(
         <div className="Container_Navbar">
@@ -17,6 +21,25 @@ const Navbar = ()=>{
                </div>
             </div>
             <SearchBar/>
+            <RiVideoAddLine size={22} className={"vid_bell_Navbar"}/>
+            <div className="apps_Box">
+                <p className="appBox"></p>
+                <p className="appBox"></p>
+                <p className="appBox"></p>
+                <p className="appBox"></p>
+                <p className="appBox"></p>
+                <p className="appBox"></p>
+                <p className="appBox"></p>
+                <p className="appBox"></p>
+                <p className="appBox"></p>
+            </div>
+            <IoMdNotificationsOutline className="vid_bell_Navbar"/>   
+            <div className="Auth_cont_Navbar">
+                <p className="Auth_Btn">
+                    <BiUserCircle size={22}/>
+                    <b>Sign in</b>
+                </p>
+            </div>
         </div>
     )
 }
--- client/src/Components/Navbar/SearchBar/SearchBar.jsx
+++ client/src/Components/Navbar/SearchBar/SearchBar.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './SearchBar.css'
 import {FaSearch} from 'react-icons/fa'
+import {BsMicFill} from 'react-icons/bs'
+
 
 const SearchBar = () => {
   return (
@@ -8,7 +10,8 @@ const SearchBar = () => {
         <div className="SearchBar_Container2">
             <div className="search_div">
                 <input type="text" className='iBox_SearchBar' placeholder='Search'/>
-                <FaSearch className='searchIcon_searchBar'/>
+                <FaSearch className='searchIcon_SearchBar'/>
+                <BsMicFill className="Mic_SearchBar searchIcon_SearchBar"/>
             </div>
         </div>
     </div>
