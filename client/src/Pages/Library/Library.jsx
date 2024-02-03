f72697ab179d174e2730e09e9df65b778f01a205
Lav Kushwaha <lavkumar1224@gmail.com>
2024-02-04 04:07:25 +0530

    YouTube Librarypage

--- client/src/Pages/Library/Library.jsx
+++ client/src/Pages/Library/Library.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import './Library.css'
 import LeftSidebar from '../../Components/LeftSidebar/LeftSidebar'
 // import vid from '../../Components/Video/vid.mp4'
+import {FaHistory} from 'react-icons/fa'
+import {MdOutlineWatchLater} from 'react-icons/md'
+import {AiOutlineLike} from 'react-icons/ai'
+import WHLVideoList from '../../Components/WHL/WHLVideoList'
 
 
 const Library = () => {
@@ -38,15 +42,59 @@ const Library = () => {
       Uploder: "abc",
       description: "description of  video 3",
     },
+
   ];
 
   return (
     <div className='container_Pages_App'>
         <LeftSidebar/>
+
+        {/* History */}
         <div className="container2_Pages_App">
             <div className="container_libraryPage">
-              
-            </div>
+               <h1 className="title_container_LibraryPage">
+                  <b>
+                    <FaHistory/>
+                  </b>
+                  <b>History</b>
+               </h1>
+               <div className="container_videoList_LibraryPage">
+                  <WHLVideoList
+                  page={"History"}
+                  videoList={vids}/>
+               </div>
+             </div>
+
+            {/* Watch Later */}
+            <div className="container_libraryPage">
+               <h1 className="title_container_LibraryPage">
+                  <b>
+                    <MdOutlineWatchLater/>
+                  </b>
+                  <b>Watch Later</b>
+               </h1>
+               <div className="container_videoList_LibraryPage">
+                  <WHLVideoList
+                  page={"Watch Later"}
+                  videoList={vids}/>
+               </div>
+           </div>
+
+           {/* Liked Video */}
+            <div className="container_libraryPage">
+               <h1 className="title_container_LibraryPage">
+                  <b>
+                    <AiOutlineLike />
+                  </b>
+                  <b>Liked Videos</b>
+               </h1>
+               <div className="container_videoList_LibraryPage">
+                  <WHLVideoList
+                  page={"Watch Later"}
+                  videoList={vids}/>
+               </div>
+           </div>
+
         </div>
     </div>
   )
