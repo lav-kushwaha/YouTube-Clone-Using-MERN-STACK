68d6278243ce40650a4b26cb023038183c4be21f
Lav Kushwaha <lavkumar1224@gmail.com>
2024-02-08 22:18:44 +0530

    Create Chanel page part 3

--- client/src/Pages/Chanel/DescribeChanel.jsx
+++ client/src/Pages/Chanel/DescribeChanel.jsx
@@ -1,36 +1,42 @@
-import React from 'react'
-import { FaEdit, FaUpload } from 'react-icons/fa'
-import './DescribeChanel.css'
-import { useSelector } from 'react-redux';
-
-const DescribeChanel = ({setEditCreateChanelBtn,Cid}) => {
-
-
+import React from "react";
+import { FaEdit, FaUpload } from "react-icons/fa";
+import { useSelector } from "react-redux";
+import "./DescribeChanel.css";
+function DecribeChanel({ setEditCreateChanelBtn, Cid,setVidUploadPage }) {
   const chanels = useSelector((state) => state?.chanelReducers);
-  console.log(chanels)
-  // const currentChanel = useSelector()
+
+  const currentChanel = chanels.filter((c) => c._id === Cid)[0];
+
+  const CurrentUser = useSelector((state) => state?.currentUserReducer);
 
   return (
-    <div className='container3_chanel'>
-       <div className="chanel_logo_chanel">
-          <b>
-            C
-          </b>
-       </div>   
-       <div className="description_chanel">
-        <b>Lav Kushwaha</b>
-        <p>StoryTeller Content Creator</p>
-       </div>
-       <p className='editbtn_chanel' onClick={()=>{setEditCreateChanelBtn(true)}}>
-            <FaEdit/>
-            <b>edit chanel</b>
-       </p>
-       <p className='uploadbtn_chanel'>
-            <FaUpload/>
-            <b>Upload Video</b>
-       </p>
+    <div className="container3_chanel">
+      <div className="chanel_logo_chanel">
+        <b>{currentChanel?.name.charAt(0).toUpperCase()}</b>
+      </div>
+      <div className="description_chanel">
+        <b> {currentChanel?.name} </b>
+        <p> {currentChanel?.desc} </p>
+      </div>
+      {CurrentUser?.result._id === currentChanel?._id && (
+        <>
+          <p
+            className="editbtn_chanel"
+            onClick={() => {
+              setEditCreateChanelBtn(true);
+            }}
+          >
+            <FaEdit />
+            <b> Edit Chanel</b>
+          </p>
+          <p className="uploadbtn_chanel" onClick={()=>setVidUploadPage(true)}>
+            <FaUpload />
+            <b> Upload Video</b>
+          </p>
+        </>
+      )}
     </div>
-  )
+  );
 }
 
-export default DescribeChanel
+export default DecribeChanel;
