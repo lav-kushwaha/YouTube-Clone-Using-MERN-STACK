3e1d4d15bd0959e80df8643751434b44eabe20a6
Lav Kushwaha <lavkumar1224@gmail.com>
2024-02-25 01:32:14 +0530

    YouTube-Save to watchlater

--- client/src/Reducers/index.js
+++ client/src/Reducers/index.js
@@ -3,17 +3,17 @@ import authReducer from "./auth";
 import chanelReducers from "./chanel";
 import currentUserReducer from "./currentUser";
 import videoReducer from "./Video";
-// import likedVideoReducer from "./likedVideo";
-// import watchLaterReducer from "./watchLater";
-// import commentReducer from "./comments";
-// import HistoryReducer from "./history";
+import likedVideoReducer from "./likedVideo";
+import watchLaterReducer from "./watchLater";
+import commentReducer from "./comments";
+import HistoryReducer from "./history";
 
 export default combineReducers({
   authReducer,
   currentUserReducer,
   chanelReducers,
   videoReducer,
-//   likedVideoReducer,
-//   watchLaterReducer,
-//   HistoryReducer,commentReducer
+  likedVideoReducer,
+  watchLaterReducer,
+  HistoryReducer,commentReducer
 });
--- server/routes/video.js
+++ server/routes/video.js
@@ -1,11 +1,10 @@
 import express from 'express'
-
 import {uploadVideo,getAllvideos} from '../controllers/video.js'
 import {likeController} from '../controllers/like.js'
 import {viewController} from '../controllers/views.js'
 import {likeVideoController,getAlllikeVideoController,deleteLikeVideoController} from '../controllers/likeVideo.js'
-// import {watchLaterController,getAllwatchLaterController,deletewatchLaterController} from '../controllers/watchLater.js'
-// import {HistoryController,getAllHistoryController,deleteHistoryController} from '../controllers/History.js'
+import {watchLaterController,getAllwatchLaterController,deletewatchLaterController} from '../controllers/watchLater.js'
+import {HistoryController,getAllHistoryController,deleteHistoryController} from '../controllers/History.js'
 import upload from '../Helpers/fileHelpers.js'
 import auth from '../middleware/auth.js' 
 
@@ -20,12 +19,12 @@ routes.post('/likeVideo',auth,likeVideoController)
 routes.get('/getAlllikeVideo',getAlllikeVideoController)
 routes.delete('/deleteLikedVideo/:videoId/:Viewer',auth,deleteLikeVideoController)
 
-// routes.post('/watchLater',auth,watchLaterController)
-// routes.get('/getAllwatchLater',getAllwatchLaterController)
-// routes.delete('/deleteWatchlater/:videoId/:Viewer',auth,deletewatchLaterController)
+routes.post('/watchLater',auth,watchLaterController)
+routes.get('/getAllwatchLater',getAllwatchLaterController)
+routes.delete('/deleteWatchlater/:videoId/:Viewer',auth,deletewatchLaterController)
 
-// routes.post('/History',auth,HistoryController)
-// routes.get('/getAllHistory',getAllHistoryController)
-// routes.delete('/deleteHistory/:userId',auth,deleteHistoryController)
+routes.post('/History',auth,HistoryController)
+routes.get('/getAllHistory',getAllHistoryController)
+routes.delete('/deleteHistory/:userId',auth,deleteHistoryController)
 
 export default routes;
