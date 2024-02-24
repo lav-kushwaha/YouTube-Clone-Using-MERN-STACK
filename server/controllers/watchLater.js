097d5226a6bcbf27b8df76330e77e9a27fc10d5a
Lav Kushwaha <lavkumar1224@gmail.com>
2024-02-25 02:15:52 +0530

    YouTube- Delete from watchlater and liked videos

--- server/controllers/watchLater.js
+++ server/controllers/watchLater.js
@@ -4,13 +4,12 @@ import mongoose from "mongoose";
 export const watchLaterController = async (req, res) => {
   const watchLaterData = req.body;
 
-  // console.log(watchLaterData);
   const addTowatchLater = new watchLater(watchLaterData);
 
   try {
     await addTowatchLater.save();
     res.status(200).json("added to watchLater");
-    // console.log("Done");
+
   } catch (error) {
     res.status(400).json(error);
   }
@@ -27,7 +26,6 @@ export const getAllwatchLaterController = async (req, res) => {
 
 export const deletewatchLaterController = async (req, res) => {
   const { videoId: videoId, Viewer: Viewer } = req.params;
-  // console.log(videoId,Viewer)
   try {
     await watchLater.findOneAndDelete({
       videoId: videoId,
