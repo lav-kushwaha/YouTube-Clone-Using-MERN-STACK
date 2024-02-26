a65bbbab9c3ff703fbd533ab7e89f7a570465770
Lav Kushwaha <lavkumar1224@gmail.com>
2024-02-27 02:04:34 +0530

    YouTube-Show video on your video and chanel page

--- client/src/Pages/Chanel/Chanel.jsx
+++ client/src/Pages/Chanel/Chanel.jsx
@@ -1,62 +1,61 @@
 import React from 'react'
-import LeftSidebar from '../../Components/LeftSidebar/LeftSidebar'
-import DescribeChanel from './DescribeChanel';
-import ShowVideoGrid from '../../Components/ShowVideoGrid/ShowVideoGrid';
+import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
-// import vid from '../../Components/Video/vid'
- 
-
-const Chanel = ({setEditCreateChanelBtn,setVidUploadPage}) => {
-
-    const {Cid} = useParams();
-
-    const vids = [
-        {
-          _id: 1,
-          video_src: "vid",
-          Chanel: "62bafe6752cea35a6c30685f",
-          title: "video 1",
-          Uploder: "abc",
-          description: "description of  video 1",
-        },
-        {
-          _id: 2,
-          video_src: "vid",
-          Chanel: "cdd",
-          title: "video 2",
-          Uploder: "abc",
-          description: "description of  video 2",
-        },
-        {
-          _id: 3,
-          video_src: "vid",
-          Chanel: "add",
-          title: "video 3",
-          Uploder: "abc",
-          description: "description of  video 3",
-        },
-        {
-          _id: 4,
-          video_src: "vid",
-          Chanel: "add",
-          title: "video 3",
-          Uploder: "abc",
-          description: "description of  video 3",
-        },
-      ];
+import LeftSidebar from '../../Components/LeftSidebar/LeftSidebar';
+import ShowVideoGrid from '../../Components/ShowVideoGrid/ShowVideoGrid';
+// import vid from "../../Components/Video/vid.mp4";
+import DecribeChanel from './DecribeChanel';
+function Chanel({setEditCreateChanelBtn,setVidUploadPage} ) {
     
-  return (
-    <div className="container_Pages_App">
+    const {Cid}=useParams();
+    const vids=useSelector(state=>state.videoReducer)?.data?.filter(q=>q?.videoChanel === Cid).reverse();
+  
+  // const vids = [
+  //   {
+  //     _id: 1,
+  //     video_src: vid,
+  //     Chanel: "62bafe6752cea35a6c30685f",
+  //     title: "video 1",
+  //     Uploder: "abc",
+  //     description: "description of  video 1",
+  //   },
+  //   {
+  //     _id: 2,
+  //     video_src: vid,
+  //     Chanel: "cdd",
+  //     title: "video 2",
+  //     Uploder: "abc",
+  //     description: "description of  video 2",
+  //   },
+  //   {
+  //     _id: 3,
+  //     video_src: vid,
+  //     Chanel: "add",
+  //     title: "video 3",
+  //     Uploder: "abc",
+  //     description: "description of  video 3",
+  //   },
+  //   {
+  //     _id: 4,
+  //     video_src: vid,
+  //     Chanel: "add",
+  //     title: "video 3",
+  //     Uploder: "abc",
+  //     description: "description of  video 3",
+  //   },
+  // ];
+    return (
+        <div className="container_Pages_App">
           <LeftSidebar />
           <div className="container2_Pages_App">
-            <DescribeChanel 
+            <DecribeChanel 
             Cid={Cid}
             setVidUploadPage={setVidUploadPage}
             setEditCreateChanelBtn={setEditCreateChanelBtn}/>
-           <ShowVideoGrid vids={vids}/>
+            <ShowVideoGrid vids={vids} />
           </div>
-        </div> 
-  )
+        </div>
+      );
 }
 
 export default Chanel
