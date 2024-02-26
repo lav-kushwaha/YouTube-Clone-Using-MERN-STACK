ad3aff157329c0d9d97951fd2672f42c4867cb5f
Lav Kushwaha <lavkumar1224@gmail.com>
2024-02-27 02:20:12 +0530

    YouTube-search page

--- client/src/Components/AllRoutes.jsx
+++ client/src/Components/AllRoutes.jsx
@@ -1,30 +1,33 @@
-import React from 'react'
-import {Routes,Route} from 'react-router-dom';
-import Home from '../Pages/Home/Home';  
-import Library from '../Pages/Library/Library';
-import YourVideo from '../Pages/YourVideo/YourVideo'
-import LikedVideo from '../Pages/LikedVideo/LikedVideo'
-import WatchHistory from '../Pages/WatchHistory/WatchHistory'
-import WatchLater from '../Pages/WatchLater/WatchLater'
-import VideoPage from '../Pages/VideoPage/VideoPage';
-import Chanel from '../Pages/Chanel/Chanel';
-
-const AllRoutes = ({setEditCreateChanelBtn,setVidUploadPage}) => {
+import React from "react";
+import Home from "../Pages/Home/Home";
+import { Routes, Route } from "react-router-dom";
+import Library from "../Pages/Library/Library";
+import YourVideo from "../Pages/YourVideo/YourVideo";
+import WatchHistory from "../Pages/WatchHistory/WatchHistory";
+import WatchLater from "../Pages/WatchLater/WatchLater";
+import LikedVideo from "../Pages/LikedVideo/LikedVideo";
+import VideoPage from "../Pages/VideoPage/VideoPage";
+import Chanel from "../Pages/Chanel/Chanel";
+import Search from "../Pages/Search/Search";
+function AllRoutes({ setEditCreateChanelBtn,setVidUploadPage }) {
   return (
     <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/library' element={<Library/>}/>
-        <Route path='/history' element={<WatchHistory/>}/>
-        <Route path='/watchlater' element={<WatchLater/>}/>
-        <Route path='/likedvideo' element={<LikedVideo/>}/>
-        <Route path='/yourvideos' element={<YourVideo/>}/>
-        <Route path='/videopage/:vid' element={<VideoPage/>}/>
-        <Route path='/chanel/:Cid' element={
-        <Chanel 
-        setVidUploadPage={setVidUploadPage}
-        setEditCreateChanelBtn={setEditCreateChanelBtn}/>}/>
+      <Route path="/" element={<Home />} />
+      <Route path="/library" element={<Library />} />
+      <Route path="/history" element={<WatchHistory />} />
+      <Route path="/watchlater" element={<WatchLater />} />
+      <Route path="/likedvideo" element={<LikedVideo />} />
+      <Route path="/yourvideos" element={<YourVideo />} />
+      <Route path="/videopage/:vid" element={<VideoPage />} />
+      <Route path="/seacrh/:searchQuery" element={<Search />} />
+      <Route
+        path="/chanel/:Cid"
+        element={<Chanel 
+          setVidUploadPage={setVidUploadPage}
+          setEditCreateChanelBtn={setEditCreateChanelBtn} />}
+      />
     </Routes>
-  )
+  );
 }
 
-export default AllRoutes
+export default AllRoutes;
--- client/src/Components/Navbar/SearchBar/SearchBar.jsx
+++ client/src/Components/Navbar/SearchBar/SearchBar.jsx
@@ -1,29 +1,45 @@
-import React, { useState } from 'react'
-import './SearchBar.css'
-import {FaSearch} from 'react-icons/fa'
-import {BsMicFill} from 'react-icons/bs'
-import SearchList from './SearchList'
-
-const SearchBar = () => {
-  const [searchQuery,setSearchQuery] = useState("")
-  const [searchListA,setSearchList] = useState(false);
-  const TitleArray = ["Video1","video2","Animation Video","Movies",].filter(q =>q.toLowerCase().includes(searchQuery));
-
-  return (
-    <div className='SearchBar_Container'>
+import React from "react";
+import "./SearchBar.css";
+import { BsMicFill } from "react-icons/bs";
+import { FaSearch } from "react-icons/fa";
+import SearchList from "./SearchList";
+import {useState} from 'react'
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+function SearchBar() {
+  const [searchQuery, setSearchQuery] = useState("");
+const [seachListA, setSeachList] = useState(false)
+const TitleArray = useSelector(s=>s?.videoReducer)
+?.data?.filter(q=> q?.videoTitle.toUpperCase().includes(searchQuery?.toUpperCase())).map(m=>m?.videoTitle)
+// console.log(TitleArray)
+// const TitleArray=["video1","Video2","Animation video","Movies"].filter(q=>q.toUpperCase().includes(searchQuery.toUpperCase()));
+ return (
+    <>
+      <div className="SearchBar_Container">
         <div className="SearchBar_Container2">
-            <div className="search_div">
-                <input type="text" className='iBox_SearchBar' placeholder='Search' value={searchQuery} onChange={(e)=>setSearchQuery(e.target.value)} onClick={(e)=>setSearchList(true)}/>
-                <FaSearch className='searchIcon_SearchBar' onClick={(e)=>setSearchList(false)}/>
-                <BsMicFill className="Mic_SearchBar searchIcon_SearchBar"/>
-                {
-                  searchQuery&&searchListA&&
-                  <SearchList setSearchQuery={setSearchQuery} TitleArray={TitleArray}/>
-                }
-            </div>
+          <div className="search_div">
+            <input type="text" className="iBox_SearchBar" placeholder="Search" 
+              onChange={e=>setSearchQuery(e.target.value)}
+              value={searchQuery}
+              onClick={e=>setSeachList(true)}
+            />
+            <Link to={`/seacrh/${searchQuery}`}>
+              <FaSearch className="searchIcon_SearchBar" 
+              onClick={e=>setSeachList(false)}
+            />
+            </Link>
+            <BsMicFill className="Mic_SearchBar" />
+            {searchQuery&& seachListA &&
+              <SearchList
+              setSearchQuery={setSearchQuery}
+              TitleArray={TitleArray}
+              />
+            }
+          </div>
         </div>
-    </div>
-  )
+      </div>
+    </>
+  );
 }
 
-export default SearchBar
+export default SearchBar;
