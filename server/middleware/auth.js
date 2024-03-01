8cc2daffc8f386b052733e3ca37eeac4fdc6ef35
Lav Kushwaha <lavkumar1224@gmail.com>
2024-03-02 02:32:04 +0530

    YouTube- Creating middleware on server side

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -3,7 +3,6 @@ import jwt from 'jsonwebtoken'
 const auth =(req,res,next)=>{
     try {
         const token = req.headers.authorization.split(" ")[1];
-
         let decodeData= jwt.verify(token,process.env.JWT_SECRET)
          req.userId=decodeData?.id
         next();        
