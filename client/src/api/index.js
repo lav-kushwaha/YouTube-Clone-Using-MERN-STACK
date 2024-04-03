3fff6290921974e3d879efa258385461c285c295
Lav Kushwaha <lavkumar1224@gmail.com>
2024-04-03 10:00:06 +0530

    client api

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const API = axios.create({ baseURL: `http://localhost:5500/` });
-const API = axios.create({ baseURL: `youtubeclone-ebon.vercel.app/` });
+const API = axios.create({ baseURL: `https://youtubeclone-ebon.vercel.app/`})
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("Profile")) {
