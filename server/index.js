061c365a7d78f6029976f8446cbf824f583dce55
Lav Kushwaha <lavkumar1224@gmail.com>
2024-04-03 09:47:03 +0530

    server

--- server/index.js
+++ server/index.js
@@ -13,14 +13,14 @@ dontenv.config()
 
 
 const app=express()
-// app.use(cors())
-app.use(cors(
-    {
-        origin:["https://youtubecloneweb.vercel.app"],
-        methods:["POST","GET"],
-        credentials:"true"
-    }
-));
+app.use(cors())
+// app.use(cors(
+//     {
+//         origin:["https://youtubecloneweb.vercel.app"],
+//         methods:["POST","GET"],
+//         credentials:"true"
+//     }
+// ));
 app.use(express.json({limit:"30mb",extended:true}))
 app.use(express.urlencoded({limit:"30mb",extended:true}))
 app.use('/uploads',express.static(path.join('uploads')))
