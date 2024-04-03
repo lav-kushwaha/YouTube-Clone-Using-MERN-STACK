f7a13f1e7bfa961ebfe894872b986890f29e628d
Lav Kushwaha <lavkumar1224@gmail.com>
2024-04-03 08:48:45 +0530

    Deployment Setup

--- server/index.js
+++ server/index.js
@@ -13,7 +13,14 @@ dontenv.config()
 
 
 const app=express()
-app.use(cors())
+// app.use(cors())
+app.use(cors(
+    {
+        origin:["https://youtubeclone.vercel.app"],
+        methods:["POST","GET"],
+        credentials:"true"
+    }
+));
 app.use(express.json({limit:"30mb",extended:true}))
 app.use(express.urlencoded({limit:"30mb",extended:true}))
 app.use('/uploads',express.static(path.join('uploads')))
